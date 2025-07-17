4e31312a4b66017113748b710d72b909fc127bbc
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-17 04:03:59 +0100

    fix: learn types, hoisting, scope and other ways of creating functions

--- playground1/custom-functions/way-to-make-function.js
+++ playground1/custom-functions/way-to-make-function.js
@@ -25,9 +25,60 @@
 //   return inches * 2.54;
 // };
 
-const inchToCM = (inches) => inches * 2.54;
+// const inchToCM = (inches) => inches * 2.54;
+// const add2 = (a, b = 3) => a + b;
 
-// console.log(inchToCM(3));
+// function makeBaby(first, last){
+//     const baby = {
+//         name: `${first} ${last}`,
+//         age: 10
+//     }
+//     return baby;
+// }
 
-const add2 = (a, b = 3) => a + b;
-console.log(add2(2, 4));
+// const makeBaby = (first, last) => {
+//   return {
+//     name: `${first} ${last}`,
+//     age: 10,
+//   };
+// };
+const makeBaby = (first, last) => ({ name: `${first} ${last}`, age: 10 });
+
+// IIFE
+// Immediately Invoked Function Expression
+(function(age) {
+  return `You are cool and age ${age}`;
+})(10);
+
+// Methods!!
+const gid = {
+  name: "Gideon Odiokine",
+  //   Method!
+  sayHi: function() {
+    return "Hey Gideon";
+  },
+  //   Short hand method
+  yellHi() {
+    console.log("HIIIIIIIIIIIIIIII");
+  },
+  //   Arrow function
+  wisperHi: () => {
+    console.log("Wisper hiiiiiiiiii");
+  },
+};
+// console.log(gid.yellHi());
+
+// Callback function
+// click callback
+const button = document.querySelector(".clickMe");
+// function handleClick(){
+//     console.log("Hello!!!!!!!!!!")
+// }
+button.addEventListener("click", function() {
+  console.log("Nice Job");
+});
+
+// Timer callback
+setTimeout(() => {
+  console.log("Hello time to eat");
+}, 1000);
---
+++ playground1/scope.js
@@ -0,0 +1,46 @@
+// const age = 100;
+
+// function go() {
+//   const age = 200;
+//   let hair = "Blonde";
+//   console.log(age);
+//   console.log(hair);
+// }
+// console.log(age);
+// go();
+
+// Block Scope
+// if(1 === 1){
+//     var cool = true;
+// }
+// function isCool(name) {
+//   let cool;
+//   if (name === "gid") {
+//     cool = true;
+//   }
+//   return cool;
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const dog = "snickers";
+
+// function logDog(dog) {
+//   console.log(dog);
+// }
+
+// function go() {
+//   const dog = "sunny";
+//   logDog("Rufus");
+// }
+// go();
+
+// function sayHi(name){
+//     function yell(){
+//         console.log(name.toUpperCase())
+//     }
+//     yell()
+// }
+
