d71f21d7098ebb77f374609050b3ff5b0310fa69
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-07-25 17:22:07 +0100

    feat: add initial implementation of Tabs component with accessibility features

---
+++ exercises/36 - Tabs/tabs.js
@@ -0,0 +1,33 @@
+const tabs = document.querySelector(".tabs");
+const tabButtons = tabs.querySelectorAll('[role="tab"');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"');
+
+function handleTabClick(e) {
+  // hide all tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  // mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute("aria-selected", false);
+  });
+  // mark the clicked tab as selected
+  e.currentTarget.setAttribute("aria-selected", true);
+  // find the associated tabPanel and show it!
+  const { id } = e.currentTarget;
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener("click", handleTabClick)
+);
+
+/*
+    METHOD 1
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  console.log(tabPanel);
+  tabPanel.hidden = false;
+  */
+
+// METHOD 2 - find in the array of tabPanels
